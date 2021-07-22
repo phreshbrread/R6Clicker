6f70de99dabf361e30ba18d516ff0fb10a7b6cb0
Brad Parker <crustydog9@gmail.com>
2021-07-22 18:33:48 +1000

    Add files via upload

--- src/R6Clicker.cs
+++ src/R6Clicker.cs
@@ -1,16 +1,15 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
-using WindowsInput;
-using WindowsInput.Native;
+using System.IO;
 
 namespace R6Clicker
 {
     public partial class R6Clicker : Form
     {
         // Declare variables
-        public int mouseClickX = 1200;
-        public int mouseClickY = 985;
+        public int mouseClickX;
+        public int mouseClickY;
 
         public string ResTextBoxText;
 
@@ -18,7 +17,50 @@ namespace R6Clicker
         {
             InitializeComponent();
 
+            if (File.Exists("settings.ini") == false)
+            {
+                ClickTimer.Interval = 2000;
+                mouseClickX = 1200;
+                mouseClickY = 985;
+
+                CustomPosBoxX.Text = Convert.ToString(200);
+                CustomPosBoxY.Text = Convert.ToString(200);
+
+                IntervalBox.Text = ClickTimer.Interval.ToString();
+
+                WriteSets();
+            }
+
+            // Read each line of the file into a string array.
+            // Each element of the array is one line of the file.
+            string[] lines = File.ReadAllLines("settings.ini");
+
+            // Set values to those from settings file
+            try
+            {
+                ClickTimer.Interval = Convert.ToInt32(lines[0]); // Set value to Int32 from lines variable
+            }
+            catch (FormatException)
+            {
+                File.Delete("settings.ini");
+                MessageBox.Show("An error with the settings file was found. Please relaunch the application.", "Settings File Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Application.Exit();
+            }
+            Int32.TryParse(lines[1], out mouseClickX);
+            Int32.TryParse(lines[2], out mouseClickY);
+
             IntervalBox.Text = ClickTimer.Interval.ToString();
+
+            if (Convert.ToInt32(lines[3]) <= 0 || Convert.ToInt32(lines[4]) <= 0)
+            {
+                CustomPosBoxX.Text = "200";
+                CustomPosBoxY.Text = "200";
+            }
+            else
+            {
+                CustomPosBoxX.Text = lines[3];
+                CustomPosBoxY.Text = lines[4];
+            }
 
             SetMousePos(mouseClickX, mouseClickY);
         }
@@ -35,20 +77,11 @@ namespace R6Clicker
 
         private const int MOUSEEVENTF_LEFTDOWN = 0x02;
         private const int MOUSEEVENTF_LEFTUP = 0x04;
-
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
-
-        // Declare some keyboard keys as constants with its respective code
-        public const int KEYEVENTF_EXTENDEDKEY = 0x0001; //Key down flag
-        public const int KEYEVENTF_KEYUP = 0x0002; //Key up flag
-        public const int VK_RETURN = 0x0D; //Enter key code
-
         #endregion
 
         public void SetMousePos(int x, int y) // Set the mouse position based on the MouseClickX and Y variables
         {
-            ResTextBoxText = x + " x " + y;
+            ResTextBoxText = x + ", " + y;
 
             mouseClickX = x;
             mouseClickY = y;
@@ -58,7 +91,6 @@ namespace R6Clicker
 
         private void ClickTimer_Tick(object sender, EventArgs e) // Run every time the timer ticks.
         {
-            
             System.Diagnostics.Process[] p = System.Diagnostics.Process.GetProcessesByName("RainbowSix"); // Search for R6 process
             if (p.Length > 0) // Check if window was found
             {
@@ -66,12 +98,24 @@ namespace R6Clicker
             }
             System.Diagnostics.Debug.WriteLine(p.Length.ToString());
 
-            SetCursorPos(mouseClickX, mouseClickY);
             Refresh();
-            
+
+            SetCursorPos(mouseClickX, mouseClickY);
             mouse_event(MOUSEEVENTF_LEFTDOWN, mouseClickX, mouseClickY, 0, 0);
             System.Threading.Thread.Sleep(50);
             mouse_event(MOUSEEVENTF_LEFTUP, mouseClickX, mouseClickY, 0, 0);
+            System.Threading.Thread.Sleep(50);
+        }
+
+        void WriteSets()
+        {
+            if (Convert.ToInt32(CustomPosBoxX.Text) <= 0 || Convert.ToInt32(CustomPosBoxY.Text) <= 0)
+            {
+                CustomPosBoxX.Text = "200";
+                CustomPosBoxY.Text = "200";
+            }
+
+            FileRW.WriteSettings(Convert.ToInt32(IntervalBox.Text), mouseClickX, mouseClickY, Convert.ToInt32(CustomPosBoxX.Text), Convert.ToInt32(CustomPosBoxY.Text));
         }
 
         #region Button clicks
@@ -79,13 +123,15 @@ namespace R6Clicker
         {
             // Try to convert the contents of the click timer box into an integer, otherwise do nothing.
             Int32.TryParse(IntervalBox.Text, out int interval);
-            if(interval > 0)
+            if (interval > 0)
             {
                 ClickTimer.Interval = interval;
+                WriteSets();
                 ClickTimer.Enabled = true;
-            } else
+            }
+            else
             {
-                MessageBox.Show("Please enter a value above 0", "Input Out Of Range", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show("Please enter a value within 0 to 2147483647.", "Input Out Of Range", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
 
@@ -94,7 +140,6 @@ namespace R6Clicker
             ClickTimer.Enabled = false;
         }
 
-        // TODO Fix 1440 & 768
         private void Res4k_Click(object sender, EventArgs e)
         {
             mouseClickX = 2400;
