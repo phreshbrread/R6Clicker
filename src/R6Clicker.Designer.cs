1a668a7eb7e4ce2f0cd0731ee3639085e5be679f
Brad Parker <crustydog9@gmail.com>
2021-12-05 14:21:41 +1100

    Add files via upload

--- src/R6Clicker.Designer.cs
+++ src/R6Clicker.Designer.cs
@@ -36,17 +36,20 @@
             this.ClickTimer = new System.Windows.Forms.Timer(this.components);
             this.ClickIntervalLabel = new System.Windows.Forms.Label();
             this.IntervalBox = new System.Windows.Forms.TextBox();
-            this.Res4k = new System.Windows.Forms.Button();
-            this.Res1080p = new System.Windows.Forms.Button();
-            this.ResolutionTextBox = new System.Windows.Forms.TextBox();
-            this.Res768p = new System.Windows.Forms.Button();
-            this.Res1440p = new System.Windows.Forms.Button();
+            this.MousePosTextBox = new System.Windows.Forms.TextBox();
             this.MousePosLabel = new System.Windows.Forms.Label();
             this.CustomPosBoxX = new System.Windows.Forms.TextBox();
             this.CustomPosLabel = new System.Windows.Forms.Label();
             this.SetButton = new System.Windows.Forms.Button();
             this.CustomPosBoxY = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.Res1080p = new System.Windows.Forms.Button();
+            this.Res4k = new System.Windows.Forms.Button();
+            this.Res1440p = new System.Windows.Forms.Button();
+            this.Res768p = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -121,59 +124,15 @@
             this.IntervalBox.Size = new System.Drawing.Size(633, 38);
             this.IntervalBox.TabIndex = 2;
             // 
-            // Res4k
+            // MousePosTextBox
             // 
-            this.Res4k.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Res4k.Location = new System.Drawing.Point(659, 91);
-            this.Res4k.Name = "Res4k";
-            this.Res4k.Size = new System.Drawing.Size(220, 153);
-            this.Res4k.TabIndex = 3;
-            this.Res4k.Text = "2400, 1980 (4K)";
-            this.Res4k.UseVisualStyleBackColor = true;
-            this.Res4k.Click += new System.EventHandler(this.Res4k_Click);
-            // 
-            // Res1080p
-            // 
-            this.Res1080p.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Res1080p.Location = new System.Drawing.Point(659, 250);
-            this.Res1080p.Name = "Res1080p";
-            this.Res1080p.Size = new System.Drawing.Size(220, 154);
-            this.Res1080p.TabIndex = 5;
-            this.Res1080p.Text = "1200, 985 (1080p)";
-            this.Res1080p.UseVisualStyleBackColor = true;
-            this.Res1080p.Click += new System.EventHandler(this.Res1080p_Click);
-            // 
-            // ResolutionTextBox
-            // 
-            this.ResolutionTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ResolutionTextBox.Location = new System.Drawing.Point(659, 43);
-            this.ResolutionTextBox.Name = "ResolutionTextBox";
-            this.ResolutionTextBox.ReadOnly = true;
-            this.ResolutionTextBox.Size = new System.Drawing.Size(446, 38);
-            this.ResolutionTextBox.TabIndex = 6;
-            this.ResolutionTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // Res768p
-            // 
-            this.Res768p.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Res768p.Location = new System.Drawing.Point(885, 250);
-            this.Res768p.Name = "Res768p";
-            this.Res768p.Size = new System.Drawing.Size(220, 154);
-            this.Res768p.TabIndex = 8;
-            this.Res768p.Text = "900, 690 (768p)";
-            this.Res768p.UseVisualStyleBackColor = true;
-            this.Res768p.Click += new System.EventHandler(this.Res768p_Click);
-            // 
-            // Res1440p
-            // 
-            this.Res1440p.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Res1440p.Location = new System.Drawing.Point(885, 91);
-            this.Res1440p.Name = "Res1440p";
-            this.Res1440p.Size = new System.Drawing.Size(220, 153);
-            this.Res1440p.TabIndex = 7;
-            this.Res1440p.Text = "1600, 1325 (1440p)";
-            this.Res1440p.UseVisualStyleBackColor = true;
-            this.Res1440p.Click += new System.EventHandler(this.Res1440p_Click);
+            this.MousePosTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MousePosTextBox.Location = new System.Drawing.Point(659, 43);
+            this.MousePosTextBox.Name = "MousePosTextBox";
+            this.MousePosTextBox.ReadOnly = true;
+            this.MousePosTextBox.Size = new System.Drawing.Size(446, 38);
+            this.MousePosTextBox.TabIndex = 6;
+            this.MousePosTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // MousePosLabel
             // 
@@ -181,9 +140,9 @@
             this.MousePosLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.MousePosLabel.Location = new System.Drawing.Point(653, 9);
             this.MousePosLabel.Name = "MousePosLabel";
-            this.MousePosLabel.Size = new System.Drawing.Size(351, 31);
+            this.MousePosLabel.Size = new System.Drawing.Size(199, 31);
             this.MousePosLabel.TabIndex = 4;
-            this.MousePosLabel.Text = "Mouse Position - Resolution";
+            this.MousePosLabel.Text = "Mouse Position";
             // 
             // CustomPosBoxX
             // 
@@ -224,21 +183,90 @@
             this.CustomPosBoxY.TabIndex = 13;
             this.CustomPosBoxY.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(653, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 31);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Screen Resolution";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.Res1080p, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.Res4k, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.Res1440p, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.Res768p, 1, 1);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(659, 125);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(446, 279);
+            this.tableLayoutPanel2.TabIndex = 15;
+            // 
+            // Res1080p
+            // 
+            this.Res1080p.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Res1080p.Location = new System.Drawing.Point(3, 142);
+            this.Res1080p.Name = "Res1080p";
+            this.Res1080p.Size = new System.Drawing.Size(217, 124);
+            this.Res1080p.TabIndex = 9;
+            this.Res1080p.Text = "1080p";
+            this.Res1080p.UseVisualStyleBackColor = true;
+            this.Res1080p.Click += new System.EventHandler(this.Res1080p_Click_1);
+            // 
+            // Res4k
+            // 
+            this.Res4k.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Res4k.Location = new System.Drawing.Point(3, 3);
+            this.Res4k.Name = "Res4k";
+            this.Res4k.Size = new System.Drawing.Size(217, 124);
+            this.Res4k.TabIndex = 4;
+            this.Res4k.Text = "4K";
+            this.Res4k.UseVisualStyleBackColor = true;
+            this.Res4k.Click += new System.EventHandler(this.Res4k_Click_1);
+            // 
+            // Res1440p
+            // 
+            this.Res1440p.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Res1440p.Location = new System.Drawing.Point(226, 3);
+            this.Res1440p.Name = "Res1440p";
+            this.Res1440p.Size = new System.Drawing.Size(217, 124);
+            this.Res1440p.TabIndex = 8;
+            this.Res1440p.Text = "1440p";
+            this.Res1440p.UseVisualStyleBackColor = true;
+            this.Res1440p.Click += new System.EventHandler(this.Res1440p_Click_1);
+            // 
+            // Res768p
+            // 
+            this.Res768p.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Res768p.Location = new System.Drawing.Point(226, 142);
+            this.Res768p.Name = "Res768p";
+            this.Res768p.Size = new System.Drawing.Size(217, 124);
+            this.Res768p.TabIndex = 10;
+            this.Res768p.Text = "768p";
+            this.Res768p.UseVisualStyleBackColor = true;
+            this.Res768p.Click += new System.EventHandler(this.Res768p_Click_1);
+            // 
             // R6Clicker
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1117, 521);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.CustomPosBoxY);
             this.Controls.Add(this.SetButton);
             this.Controls.Add(this.CustomPosBoxX);
             this.Controls.Add(this.CustomPosLabel);
-            this.Controls.Add(this.Res768p);
-            this.Controls.Add(this.Res1440p);
-            this.Controls.Add(this.ResolutionTextBox);
-            this.Controls.Add(this.Res1080p);
+            this.Controls.Add(this.MousePosTextBox);
             this.Controls.Add(this.MousePosLabel);
-            this.Controls.Add(this.Res4k);
             this.Controls.Add(this.IntervalBox);
             this.Controls.Add(this.ClickIntervalLabel);
             this.Controls.Add(this.tableLayoutPanel1);
@@ -247,6 +275,7 @@
             this.Name = "R6Clicker";
             this.Text = "R6Clicker";
             this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -259,11 +288,7 @@
         private System.Windows.Forms.Timer ClickTimer;
         private System.Windows.Forms.Label ClickIntervalLabel;
         private System.Windows.Forms.TextBox IntervalBox;
-        private System.Windows.Forms.Button Res4k;
-        private System.Windows.Forms.Button Res1080p;
-        private System.Windows.Forms.TextBox ResolutionTextBox;
-        private System.Windows.Forms.Button Res768p;
-        private System.Windows.Forms.Button Res1440p;
+        private System.Windows.Forms.TextBox MousePosTextBox;
         private System.Windows.Forms.Label MousePosLabel;
         private System.Windows.Forms.TextBox CustomPosBoxX;
         private System.Windows.Forms.Label CustomPosLabel;
@@ -271,5 +296,11 @@
         private System.Windows.Forms.Button SetButton;
         private System.Windows.Forms.TextBox CustomPosBoxY;
         private System.Windows.Forms.Button HelpButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button Res1080p;
+        private System.Windows.Forms.Button Res4k;
+        private System.Windows.Forms.Button Res1440p;
+        private System.Windows.Forms.Button Res768p;
     }
 }
